131437e169fe449b46f8f0ef6ed44805e11f1a18
Elena-63 <domanova>
2021-06-05 10:09:59 +0400

    Update Movie.java

--- src/main/java/ru/netology/domain/Movie.java
+++ src/main/java/ru/netology/domain/Movie.java
@@ -4,12 +4,34 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class Movie {
     private int id;
     private String imageUrl;
     private String name;
     private String genre;
+
+    public Movie(int id, String imageUrl, String name, String genre) {
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.name = name;
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
 }
